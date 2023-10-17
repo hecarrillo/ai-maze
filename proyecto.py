41da190998147d6d49eefb6b40901f516e1d2d75
Jose Angel Espinosa <j.a.e.b.1999a@gmail.com>
2023-10-17 14:47:03 -0600

    P: Deleted Practice-3 code

--- proyecto.py
+++ proyecto.py
@@ -51,15 +51,9 @@ class MapApp(wx.Frame):
             self.buttons.append(button_row)
 
         # Add the "Finish Editing and Start Playing" button at the bottom
-        self.finish_btn = wx.Button(panel, label="Finish Editing")
-        self.finish_btn.Bind(wx.EVT_BUTTON, self.on_finish_editing)
+        self.finish_btn = wx.Button(panel, label="Solve")
+        self.finish_btn.Bind(wx.EVT_BUTTON, self.solve_game)
         grid.Add(self.finish_btn, 0, wx.EXPAND)
-
-        # Add the auto solve button that is disabled until the user finishes editing
-        self.auto_solve_btn = wx.Button(panel, label="Auto Solve")
-        self.auto_solve_btn.Bind(wx.EVT_BUTTON, self.auto_solve)
-        grid.Add(self.auto_solve_btn, 10, wx.EXPAND)
-        self.auto_solve_btn.Disable()
 
         panel.SetSizer(grid)
         self.Centre()
@@ -98,62 +92,21 @@ class MapApp(wx.Frame):
                 TERRAINS[selected_terrain]["color"])
             self.buttons[i][j].Refresh()
         dlg.Destroy()
-    def on_finish_editing(self, _):
-        self.start_masking()
+    def solve_game(self, _):
         self.Refresh()
         self.Update()
-        dlg = wx.SingleChoiceDialog(
-            self, 'Choose your character:', 'Character Selection', list(CHARACTERS.keys()))
-        if dlg.ShowModal() == wx.ID_OK:
-            self.selected_character = dlg.GetStringSelection()
-            self.total_cost = 0
-            self.path = []
-            self.finish_btn.Disable()
-        dlg.Destroy()
-
-        self.auto_solve_btn.Enable()
-    def solve(self, algorithm):
-        # Solve the map using the selected algorithm
-        self.DIRECTIONS = []
-        self.select_direction_priority()
-        if algorithm == "DFS":
-            self.solve_dfs()
-        elif algorithm == "BFS":
-            self.solve_bfs()
-        elif algorithm == "Iterative DFS":
-            self.solve_iterative_dfs()
-        elif algorithm == "A*":
-            self.solve_a_star()
-        self.select_plot_mode()
-        self.unmask_map(self.map_data, self.buttons)
-        self.highlight_path()
-
+        self.DIRECTIONS = DIRECTIONS
+        self.solve_a_star()
+        #self.highlight_path()
+        
     """USER ACTIONS UTILS"""
-    def handle_masked_click(self, i, j):
-        if abs(i - self.current_position[0]) + abs(j - self.current_position[1]) == 1 and self.map_data[i][j][1] not in ['I', 'V']:
-            terrain_value, _ = self.map_data[i][j]
-            terrain_name = [name for name, attributes in TERRAINS.items(
-            ) if attributes["value"] == terrain_value][0]
-            move_cost = CHARACTERS[self.selected_character][terrain_name]
-            if move_cost < 1000:  # Ensure the character can move through the terrain
-                self.total_cost += move_cost
-                self.path.append((i, j))
-                if self.map_data[i][j][1] == 'X':
-                    self.handle_game_over()
-                else:
-                    self.handle_valid_move(i, j)
-                    self.current_position = (i, j)
-                    self.unmask_surroundings(i, j)
-                    self.Refresh()
     def handle_unmasked_click(self, i, j, event):
         current_terrain, current_state = self.map_data[i][j]
         dlg = wx.SingleChoiceDialog(self, 'Set the cell value:', 'Edit Cell', [
                                     "Human", "Octopus", "Portal Key", "Dark Temple", "Portal"])
         if dlg.ShowModal() == wx.ID_OK:
             new_state = CELL_STATES[dlg.GetStringSelection()]
-            print(new_state)
             if new_state == "H":
-                print(f"{i},{j}. {self.initialHuman}")
                 if self.is_valid_cell(self.initialHuman[0], self.initialHuman[1]):
                     self.map_data[self.initialHuman[0]][self.initialHuman[1]] = (self.map_data[self.initialHuman[0]][self.initialHuman[1]][0], "")
                     self.buttons[self.initialHuman[0]][self.initialHuman[1]].SetLabel("")
@@ -196,43 +149,7 @@ class MapApp(wx.Frame):
             self.buttons[i][j].SetLabel('O')
         else:
             self.map_data[i][j] = (self.map_data[i][j][0], 'V')
-            self.buttons[i][j].SetLabel('V')
-    def select_direction_priority(self):
-        for i in range(4):
-            text = f'Choose direction #{str(i + 1)}'
-            available = [
-                op
-                for op in DIRECTION_OF_LETTER.keys()
-                if DIRECTION_OF_LETTER[op] not in self.DIRECTIONS
-            ]
-            dlg = wx.SingleChoiceDialog(
-                self, text, 'Direction Selection', available)
-            if dlg.ShowModal() == wx.ID_OK:
-                selected_letter = str(dlg.GetStringSelection())
-                print(selected_letter)
-                self.DIRECTIONS.append(DIRECTION_OF_LETTER[selected_letter])
-            dlg.Destroy()  
-    def auto_solve(self, _):
-        # Prompt the user to select to solve either by DFS or BFS
-        dlg = wx.SingleChoiceDialog(
-            self, 'Choose your algorithm:', 'Algorithm Selection', ["DFS", "BFS", "Iterative DFS", "A*"])
-        if dlg.ShowModal() == wx.ID_OK:
-            selected_algorithm = dlg.GetStringSelection()
-            print(selected_algorithm)
-            self.solve(selected_algorithm)
-        dlg.Destroy()
-    def start_masking(self):
-        self.masked = True
-        for i, row in enumerate(self.map_data):
-            for j, cell in enumerate(row):
-                _, state = cell
-                if state != 'I':
-                    self.buttons[i][j].SetBackgroundColour(MASK_COLOR)
-                    self.buttons[i][j].SetLabel('')
-                else:
-                    self.current_position = (i, j)
-                    self.path.append((i, j))
-                    wx.CallLater(100, self.unmask_surroundings, i, j)
+            self.buttons[i][j].SetLabel('V')  
 
     """MAP VALUES UTILS"""
     def get_terrain_name(self, i, j):
@@ -255,63 +172,18 @@ class MapApp(wx.Frame):
                 return attributes["color"]
         return wx.Colour(255, 255, 255)  # Default white color
     
-    """MAP VISUALIZATION UTILS"""
-    def unmask_map(self, map_data, buttons):
-        for i, row in enumerate(map_data):
-            for j, cell in enumerate(row):
-                terrain, state = cell
-                buttons[i][j].SetBackgroundColour(
-                    self.get_terrain_color(terrain))
-                buttons[i][j].SetLabel(state)
-                buttons[i][j].Refresh()
-                buttons[i][j].Update()
-    def unmask_surroundings(self, i, j):
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        for dx, dy in directions:
-            x, y = i + dx, j + dy
-            if self.is_valid_cell(x, y):
-                terrain, state = self.map_data[x][y]
-                self.buttons[x][y].SetBackgroundColour(
-                    self.get_terrain_color(terrain))
-                self.buttons[x][y].SetLabel(state)
-                self.buttons[x][y].Refresh()
-                self.buttons[x][y].Update()
-    
     """SEARCH ALGORITHMS INITIALIZATION"""
     def init_search_root(self):
         self.visited = set()
         print('initial position:', self.current_position)
         self.root = TreeNode((self.current_position[0], self.current_position[1], 'I'))
         self.root.other = "Initial Point"
-    def solve_bfs(self):
-        self.init_search_root()
-        self.bfs()
-        self.append_actions_to_nodes(self.root)
-    def solve_dfs(self):
-        self.init_search_root()
-        self.dfs(self.current_position[0], self.current_position[1], None)
-    
-    def solve_iterative_dfs(self):
-        self.init_search_root()
-        self.iterative_dfs()
-        self.append_actions_to_nodes(self.root)
     def solve_a_star(self):
         self.init_search_root()
         self.a_star()
         self.append_actions_to_nodes(self.root)
 
     """SEARCH ALGORITHM VISUALIZATION UTILS"""
-    def select_plot_mode(self):
-        dlg = wx.SingleChoiceDialog(
-            self, 'Choose hot to display the decision tree:', 'tree display mode', ["Step by step", "Decision by decision"])
-        if dlg.ShowModal() == wx.ID_OK:
-            selected_mode = dlg.GetStringSelection()
-            print(selected_mode)
-            if selected_mode == "Decision by decision":
-                self.plot_decision_tree()
-            else:
-                self.plot_step_tree()
-        dlg.Destroy() 
     def highlight_path(self):
         def traverse_tree(node):
             if node.other == "Closed Path":
@@ -326,30 +198,7 @@ class MapApp(wx.Frame):
                         return True
                 return False
         traverse_tree(self.root)
-    def plot_decision_tree(self):
-        G = nx.DiGraph()
-
-        def find_decision_maker(node):
-            if len(node.actions) > 1 or node.other == "Closed Path" or len(node.actions) == 0:
-                return node
-            for child in node.children:
-                return find_decision_maker(child)
-        def traverse_tree(node):
-            for child in node.children:
-                next_node = find_decision_maker(child)
-                if next_node is not None:
-                    G.add_edge((node.value, str(node.actions), str(node.actionsExecuted), str(node.other)), (next_node.value, str(next_node.actions), str(next_node.actionsExecuted), str(next_node.other) ))
-                    traverse_tree(next_node)
-
-        traverse_tree(self.root)
-
-        pos = hierarchy_pos(G, (self.root.value, str(self.root.actions), str(self.root.actionsExecuted), str(self.root.other)))
-        plt.figure(figsize=(10, 10))
-        labels = {node: f"Position: ({node[0][0]},{node[0][1]}), dirTaken:{node[0][2]}\nActions:{node[1]}\nActionsExecuted:{node[2]}\nOther:{node[3]}" for node in G.nodes()}
-        nx.draw(G, pos=pos, with_labels=True, labels=labels, node_size=1500, node_color="skyblue", node_shape="s", alpha=0.5, linewidths=40, )
-        plt.title("Decision Tree, decision by decision")
-        plt.show()
-
+    
     def plot_step_tree(self):
         G = nx.DiGraph()
 
@@ -413,83 +262,7 @@ class MapApp(wx.Frame):
             self.append_actions_to_nodes(child)
     
     """ SEARCH ALGORITHMS IMPLEMENTATIONS """
-    def bfs(self):
-        queue = [self.root]
-        while queue:
-            current_node = queue.pop(0)
-            x, y = current_node.value[:2]
-            self.visited.add((x, y))
-
-            if self.get_cell_value(x, y) == 'X':
-                current_node.other = "Closed Path"
-                self.label_current_cell_as_visited(x, y, current_node)
-                return True
-
-            for dx, dy in self.DIRECTIONS:
-                new_x, new_y = x + dx, y + dy
-                if self.is_valid_cell(new_x, new_y) and (new_x, new_y) not in self.visited and self.get_cell_cost(new_x, new_y) < 1000:
-                    action = self.possible_move(x, y, new_x, new_y)
-                    current_node.actions.append(action)
-                    node = TreeNode((new_x, new_y, self.direction_taken(new_x, new_y, current_node)))
-                    node.total_cost = current_node.total_cost + self.get_cell_cost(new_x, new_y)
-                    queue.append(node)
-                    current_node.add_child(node)
-                    self.visited.add((new_x, new_y))
-
-            self.label_current_cell_as_visited(x, y, current_node)
-    def dfs(self, i, j, parent_node):
-        
-        current_node = TreeNode((i, j, self.direction_taken(i,j,parent_node) )) if parent_node else self.root
-        if parent_node:
-            current_node.total_cost = parent_node.total_cost + self.get_cell_cost(i, j)
-        
-        if parent_node:
-            print('parent:', parent_node.value, 'current:', current_node.value)
-            parent_node.add_child(current_node)
-        
-        if self.get_cell_value(i, j) == 'X':
-            current_node.other = "Closed Path"
-            self.label_current_cell_as_visited(x, y, current_node)
-            return True
-
-        self.visited.add((i, j))
-
-        for dx, dy in self.DIRECTIONS:
-            x, y = i + dx, j + dy
-            if self.is_valid_cell(x, y) and (x, y) not in self.visited and self.get_cell_cost(x, y) < 1000:
-                current_node.actions.append(self.possible_move(i,j,x,y))
-        self.label_current_cell_as_visited(i, j, current_node)
-        for dx, dy in self.DIRECTIONS:
-            x, y = i + dx, j + dy
-            if self.is_valid_cell(x, y) and (x, y) not in self.visited and self.get_cell_cost(x, y) < 1000:
-                current_node.actionsExecuted.append(self.possible_move(i,j,x,y))
-                if self.dfs(x, y, current_node):
-                    return True
-        return False
-    def iterative_dfs(self):
-        stack = [self.root]
-        while stack:
-            current_node = stack.pop()
-            x, y = current_node.value[:2]
-            self.visited.add((x, y))
-
-            if self.get_cell_value(x, y) == 'X':
-                current_node.other = "Closed Path"
-                self.label_current_cell_as_visited(x, y, current_node)
-                return True
-
-            for dx, dy in self.DIRECTIONS:
-                new_x, new_y = x + dx, y + dy
-                if self.is_valid_cell(new_x, new_y) and (new_x, new_y) not in self.visited and self.get_cell_cost(new_x, new_y) < 1000:
-                    action = self.possible_move(x, y, new_x, new_y)
-                    current_node.actions.append(action)
-                    node = TreeNode((new_x, new_y, self.direction_taken(new_x, new_y, current_node)))
-                    node.total_cost = current_node.total_cost + self.get_cell_cost(new_x, new_y)
-                    stack.append(node)
-                    current_node.add_child(node)
-                    self.visited.add((new_x, new_y))
-
-            self.label_current_cell_as_visited(x, y, current_node)
+    
     def a_star(self):
         self.root.total_cost = self.manhattan_distance_to_end(self.root)
         queue = [(self.root.total_cost, self.root)]
